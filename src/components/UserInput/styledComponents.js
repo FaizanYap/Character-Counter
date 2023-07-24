1056936b9a8be870e964a3d4b5222ca3fcff4c80
faizan <faizanmohammad2151@gmail.com>
2023-07-24 06:58:57 +0000

    character counter

---
+++ src/components/UserInput/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const UserInputItem = styled.li`
+  list-style-type: none;
+`
+
+export const UserEnteredDetails = styled.div`
+  color: #0f172a;
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: bold;
+`
